5059e5b47c8bfe0f3aea4abd909eee0b510c1569
Jason Lengstorf <jason@lengstorf.com>
2020-10-07 13:03:25 -0700

    update copy

--- pages/test.js
+++ pages/test.js
@@ -3,7 +3,7 @@ import { Layout } from '../components/layout';
 export default function Index() {
   return (
     <Layout>
-      <h1>Hello Next!</h1>
+      <h1>Test Next!</h1>
       <p>Do you like my site?</p>
     </Layout>
   );
